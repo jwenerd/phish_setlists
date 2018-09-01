f5e5443b28f9480a7938dc23d7712a09fb754c48
Jared Wenerd <wenerd87@gmail.com>
2018-09-01 03:44:09 -0400

    Remove superfluous data in shell example, add previous song, more-efficient setting of next/prev song in df

--- shell.py
+++ shell.py
@@ -3,21 +3,31 @@ import IPython
 # load data
 
 df = pd.read_csv('setlist_data.csv')
-df['next_title'] = None
+df = df.drop(columns=['url','location','venue','show_rating','url','comment'])
 
-len = len(df)
+def same_set(row1, row2):
+    return row1['show_date'] == row2['show_date'] and row1['set'] == row2['set']
 
-# setup next song data based set and show
-for row in df.itertuples():
-    j = row.Index+1
-    if j >= len:
-        break        
-    next = df.iloc[j]
+# setup next solenng data based set and show
+next_titles = [] 
+prev_titles = []
 
-    next_title =  None
-    if next['show_date'] == row.show_date and next['set'] == row.set:
+for index, row in df.iterrows():
+    next = df.iloc[index + 1] if index + 1 in df.index else None
+    prev = df.iloc[index - 1] if index - 1 in df.index else None
+    next_title = prev_title = None
+    if not next is None and same_set(next,row):
         next_title = next['title']
-    df.loc[row.Index, 'next_title'] = next_title
+    if not prev is None and same_set(prev,row):
+        prev_title = prev['title']
+    
+    next_titles.append(next_title)
+    prev_titles.append(prev_title)
 
-# shell 
+df['prev_title'] = prev_titles
+df['next_title'] = next_titles
+
+
+
+# shell
 IPython.embed()
